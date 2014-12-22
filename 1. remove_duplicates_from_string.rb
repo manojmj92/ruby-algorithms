e1abb3c00ade44142b7729646ee5c47960c4d17a
Manoj M J <manojmj@freshdesk.com>
2014-12-23 00:03:51 +0530

    removing duplicates from string

---
+++ 1. remove_duplicates_from_string.rb
@@ -0,0 +1,32 @@
+class RemoveDuplicates
+
+	def initialize(string)
+		@character_array = string.scan /\w/
+		@character_hash = Hash.new
+		remove_duplicate
+		print_output
+	end
+
+private
+
+	def remove_duplicate
+		@character_array.each {|character| @character_hash[character] = true}
+ 	end
+	
+ 	def print_output
+ 		puts "Characters after duplicate removal\n"
+		@character_hash.each_key{|character| print character }
+		print_new_line
+ 	end
+
+ 	def print_new_line
+ 		print "\n"
+ 	end
+
+
+end
+
+puts "Enter the string\n"
+string = gets.chomp
+
+RemoveDuplicates.new(string)
